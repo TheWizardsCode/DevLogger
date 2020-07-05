a9b7d8ba425f954daa29f66ec0ca9722168d6d32
Ross Gardler <ross.gardler@microsoft.com>
2020-07-04 20:42:27 -0800

    version bump

--- Assets/Editor/VersionConstants.cs
+++ Assets/Editor/VersionConstants.cs
@@ -8,7 +8,7 @@ namespace org.WizardsCode.DevLogger.Editor
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "0.2.0";
+		public const string VERSION = "0.2.1";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
@@ -18,7 +18,7 @@ namespace org.WizardsCode.DevLogger.Editor
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "fe12c9a99a5ecfaf3358bb99b19225e4fd8b6217";
+		public const string GIT_COMMIT = "18940c89f160066d1edc556401fb7a97e7ddb467";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
@@ -28,6 +28,6 @@ namespace org.WizardsCode.DevLogger.Editor
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "07/05/2020 02:43:17";
+		public const string PUBLISH_TIME = "07/05/2020 04:42:13";
 	}
 }
